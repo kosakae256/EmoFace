80270df22689b191d0a62912efe617dbf74f78f7
takara2314 <takara2314_TK@outlook.jp>
2021-03-20 20:20:31 +0900

    👨🏻‍🦲 Change json to python

---
+++ data/emoticons/sad.py
@@ -0,0 +1,15 @@
+sad_emoticons = [
+    {
+        "emoticon": "(´・ω・`)",
+        "age": 25,
+        "gender": 0,
+        "is_mask": 0,
+        "glasses": 0,
+        "is_beard": 0,
+        "charm_score": 5,
+        "is_positive": 0,
+        "is_neutral": 0,
+        "is_negative": 1,
+        "smile_score": 20
+    }
+]
